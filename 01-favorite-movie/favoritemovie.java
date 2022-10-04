1ad3a6d8d6684ce215daa9399f1fe30d3f8a84fd
AP Java Period 5 <apjavaperiod5@AC1MWK0R4JC9H.local>
2022-10-04 12:03:46 -0400

    added date files

---
+++ 01-favorite-movie/favoritemovie.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of class favoritemovie here.
+ *
+ * @David Garcia
+ * @9/21/22
+ */
+public class favoritemovie
+{
+    public static void main(String[] args) {
+        System.out.println("My favorite movie is Spiderman No wayhome");
+    }
+}
+
